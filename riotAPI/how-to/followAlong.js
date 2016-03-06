ae152124bed31a3fba98f773763cc0cfa1e34341
rostun <rosa.m.tung@gmail.com>
2016-03-06 13:36:41 -0500

    api keys
    back where we started from

--- riotAPI/how-to/followAlong.js
+++ riotAPI/how-to/followAlong.js
@@ -1,11 +1,17 @@
 function searchSummoner(){
-  console.log("searching summoner");
   
 	function getSummoner(){ //get user input
-	  console.log("getting summoner");
-		return document.getElementById('summonerName').value;
+		return document.getElementById('aPromisedName').value;
 	}
 	
 	var summonerName = getSummoner(); //store user input
-  console.log("got summoner: " + summonerName); 
+	console.log(summonerName);
+	
+	var req = new XMLHttpRequest(); //create XMLHttpRequest Object (used to exchange data with a server)
+	req.open("GET", "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + summonerName + 
+					"?api_key=edd641e1-be3f-4ffa-aa56-64945420b0ba", true); //replace [dev key] with API key
+	req.send(); //Sends the request to the server (used for GET)
+    req.addEventListener('load', function(){ //listen to what Riot gives us
+		console.log(req.responseText);
+	});					
 }
