4ffbb527e4be871b7beb0ec772b2f9e9d67b7d96
rostun <rosa.m.tung@gmail.com>
2016-03-06 20:13:00 -0500

    done
    donezo

--- riotAPI/how-to/riotApiTest.js
+++ riotAPI/how-to/riotApiTest.js
@@ -16,7 +16,6 @@
 var stringSummoner = summonerName + ".id"; //summonerName.id as a string
 console.log(findValue(response, stringSummoner));*/
 
-
 function searchSummoner(){
 	function getSummoner(){ //get user input
 		return document.getElementById('aPromisedName').value;
@@ -43,6 +42,26 @@ function searchSummoner(){
 	event.preventDefault();
 }
 
+/*JSON Arrays
+function searchSummoner(){
+	function getSummoner(){ //get user input
+		return document.getElementById('aPromisedName').value;
+	}
+	
+	var summonerName = getSummoner(); //store user input
+	summonerName = summonerName.toLowerCase().trim(); //what happens when you don't do this step? missPapaya vs misspapaya, adding trim() -> misspapaya with a space at the end
+	summonerName = summonerName.replace(" ", ""); //what happens when you don't do this step? Annie Bot vs anniebot
+	
+	var req = new XMLHttpRequest(); //create XMLHttpRequest Object (used to exchange data with a server)
+	req.open("GET", "https://na.api.pvp.net/api/lol/na/v1.2/champion?freeToPlay=true&api_key=edd641e1-be3f-4ffa-aa56-64945420b0ba", true); //replace with [dev key]
+
+	req.send(null); //GET request, so not sending anything
+    req.addEventListener('load', function(){
+		var response = JSON.parse(req.responseText); //get response text from request //console.log(response[summonerName]["id"]);
+		console.log(response["champions"][0]["id"]);
+	});
+	event.preventDefault();
+}*/
 /*{"misspapaya":{
 		"id":22977975,
 		"name":"missPapaya",
